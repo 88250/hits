6c4b34315fe4ad521dbda45e27f2285d775a2b02
Daniel <845765@qq.com>
2023-06-04 21:28:48 +0800

    :art: 改进输出格式化

--- main.go
+++ main.go
@@ -89,7 +89,8 @@ func hit(c *gin.Context) {
 	locker.Lock()
 	count := writeData(fileName)
 	locker.Unlock()
-	countStr := humanize.SI(count, "")
+
+	countStr := humanize.SIWithDigits(count, 2, "")
 
 	c.Header("cache-control", "max-age=0, no-cache, no-store, must-revalidate")
 	svg := `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="88" height="20"><g shape-rendering="crispEdges"><path fill="#555" d="M0 0h37v20H0z"/><path fill="#4c1" d="M37 0h51v20H37z"/></g><g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="110">`
